d23e3f13dba7cd46c414f55ebb4d446d03a1f58f
kokoh <kokohtan@gmail.com>
2017-06-21 17:21:29 +0700

    add adv calc

--- calculator.js
+++ calculator.js
@@ -3,20 +3,44 @@
 class Calculator {
   //write your code here
   constructor () {
+    this.num= 1;
+    this.pi= 22/7;
   }
-  add () {
+  add (x) {
+    this.num += x
+    return this
   }
-  substract () {
+  substract (x) {
+    this.num -= x
+    return this
   }
-  multiply () {
+  multiply (x) {
+    this.num *= x
+    return this
   }
-  divide () {
+  divide (x) {
+    this.num /= x
+    return this
   }
   square () {
+    this.num= Math.pow(this.num,2)
+    return this
   }
   squareRoot () {
+    this.num= Math.sqrt(this.num)
+    return this
+  }
+  circle() {
+    this.num= Math.pow(this.num,2)*this.pi
+    return this
+  }
+  save() {
+    console.log(`Hasil perhitungan = ${this.num}`);
   }
 }
+
+var calculator = new Calculator()
+calculator.add(1).square(2).multiply(3).circle().save()
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
