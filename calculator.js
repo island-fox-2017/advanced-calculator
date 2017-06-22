9a88fc28ac8777fcefc7b9a83a527933f50ec0a9
Christian <email": null>
2017-06-22 14:21:10 +0700

    advance calc selesai

--- calculator.js
+++ calculator.js
@@ -2,22 +2,38 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (firstNum, secondNum) {
+    this.pertama = firstNum;
+    this.kedua = secondNum;
+    this.pi = 3.14
   }
   add () {
+    return this.pertama + this.kedua
   }
   substract () {
+    return this.pertama - this.kedua
   }
   multiply () {
+    return this.pertama * this.kedua
   }
   divide () {
+    return this.pertama / this.kedua
   }
   square () {
+    return Math.pow(this.pertama, this.kedua)
   }
   squareRoot () {
+    return [Math.sqrt(this.pertama), Math.sqrt(this.kedua)].join(', ');
+  }
+  lingkaran (r) {
+    return this.pi*(r*r)
   }
 }
-
+var bilangans = new Calculator(5, 4)
+console.log(bilangans.add());
+console.log(bilangans.square());
+console.log(bilangans.squareRoot());
+console.log(bilangans.lingkaran(4));
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
