d19e5fa42330530581e85198d3639d67a8cdb4d2
adnin31 <adnin.ris31@gmail.com>
2017-06-23 15:37:30 +0700

    update calculator terbaru

--- calculator.js
+++ calculator.js
@@ -1,39 +1,51 @@
 'use strict'
 
-class Calculator {
+class Calculator{
   //write your code here
-  constructor () {
+  constructor (consNum) {
+    this.consNum = consNum;
+  }
+  add (addNum){
+    this.consNum += addNum
+    return this
+  }
+  substract(substractNum){
+    this.consNum -= substractNum
+    return this
+  }
+  multiply(multiplyNum){
+    this.consNum *= multiplyNum
+    return this
+  }
+  divide(divideNum){
+    this.consNum /= divideNum
+    return this
+  }
+  square(squareNum){
+    this.consNum = Math.pow(this.consNum , squareNum)
+    return this
+  }
+  squareRoot(){
+    this.consNum = Math.sqrt(this.consNum)
+    return this
+  }
 
-  }
-  add (num1, num2) {
-    return num1 + num2;
-  }
-  substract (num1,num2) {
-    return num1 - num2;
-  }
-  multiply (num1,num2) {
-    return num1 * num2
-  }
-  divide (num1,num2) {
-    return num1 /num2
-  }
-  square (num1 ,num2) {
-      return Math.pow(num1, num2)
-  }
-  squareRoot (num) {
-    return Math.sqrt(num)
-  }
-  luasLingkaran(num){
+  luasLingkaran(){
     const phi = 3.14;
-    const phi2 = 22/7
-    if(num %7 == 0){
-      return phi2 * Math.pow(num, 2)
-    }  else {
-      return phi * Math.pow(num,2)
+    const phi2 = 22/7;
+    if(this.consNum % 7 == 0){
+      this.consNum = phi2 * Math.pow(this.consNum , 2)
+
+    } else {
+      this.consNum = phi * Math.pow(this.consNum , 2)
     }
+    return this
   }
+
 }
-let calculator = new Calculator();
+
+let calculator = new Calculator(4)
+console.log(calculator.add(3).substract(1).square(2).squareRoot().luasLingkaran());
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
