40d092a0f8814663866b5f52d8fce2e00460f80c
achimbaggins <achim_baggins@yahoo.com>
2017-07-01 10:37:54 +0700

    Masih eror di sqrt

--- calculator.js
+++ calculator.js
@@ -2,22 +2,44 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (inputNum) {
+    this.inputNum = inputNum;
   }
-  add () {
+  add (tambah) {
+    this.inputNum += tambah;
+    return this.inputNum
   }
-  substract () {
+  substract (kurang) {
+    this.inputNum -= kurang
+    return this.inputNum
   }
-  multiply () {
+  multiply (kali) {
+    this.inputNum *= kali
+    return this.inputNum
   }
-  divide () {
+  divide (bagi) {
+    this.inputNum /= bagi
+    return this.inputNum
   }
-  square () {
+  square (pangkat) {
+    this.inputNum = Math.pow(this.inputNum, pangkat);
+    return this.inputNum
   }
   squareRoot () {
+    return Math.sqrt(this.inputNum);
+    // return this.inputNum
   }
 }
 
+
+
+let angkaBaru = new Calculator(9)
+console.log('hasil tambah angkaBaru + 4 adalah : ' + angkaBaru.add(4));
+console.log('hasil kurang angkaBaru - 4 adalah : ' + angkaBaru.substract(4));
+console.log('hasil kali angkaBaru x 9 adalah : ' + angkaBaru.multiply(9));
+console.log('hasil bagi angkaBaru / 3 adalah : ' + angkaBaru.divide(3));
+console.log('hasil angkaBaru dipangkat 2 : ' + angkaBaru.square(2));
+// console.log('hasil akar kuadrat angkaBaru adalah s: ' angkaBaru.squareRoot());
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
